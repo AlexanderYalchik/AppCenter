42f1460c251dcc8093925b30d950b5ba5a8b926a
Alexander Yalchik <ayalchik@distillery.com>
2019-07-06 16:01:01 +0300

    no message

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
-  # bundle update --bundler
-  # bundle install
-  # bundle exec danger
-
-# cd $APPCENTER_SOURCE_DIRECTORY
-# npm install -g danger
-# swift build
-# swift run danger-swift ci
-
-#!/usr/bin/env bash
   bundle install
   bundle exec danger
