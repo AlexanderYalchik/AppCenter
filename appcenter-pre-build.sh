bb7db76bb6fb5ec93bc29a60f641bf349fdc669d
Alexander Yalchik <ayalchik@distillery.com>
2019-07-06 16:56:18 +0300

    no message

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-  bundle update --bundler
-  bundle install
-  bundle exec danger
+  # bundle update --bundler
+  # bundle install
+  # bundle exec danger
+
+  rbenv global 2.3.1 && gem install danger --version '~> 5.0' && danger
