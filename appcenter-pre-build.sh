64b8561b01a9ff3d8c6f78a28135663835db97cd
Alexander Yalchik <ayalchik@distillery.com>
2019-07-07 14:35:19 +0300

    no message

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
-cd $APPCENTER_SOURCE_DIRECTORY
-npm config delete prefix
-npm install --save @babel/polyfill
-npm install -g danger
-swift build
-swift run danger-swift ci
+# cd $APPCENTER_SOURCE_DIRECTORY
+# npm install -g danger
+# swift build
+# swift run danger-swift ci
+
+# Setup
+npm install -g danger@alpha           # Get DangerJS
+brew install marathon-swift           # Install the SwiftPM app installer
+marathon install danger/danger-swift  # Install danger-swift locally
+
+# Script
+danger process danger-swift           # Run Danger
